85eeae6c910b77c0fa035961dc4748ad31f2bcce
victoresg <victoresg9@gmail.com>
2023-05-31 18:27:12 -0300

    Server controller and repository

---
+++ src/server/controller/todo.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { z as schema } from "zod";
+import { todoRepository } from "@server/repository/todo";
+
+async function get(req: NextApiRequest, res: NextApiResponse) {
+  const query = req.query;
+  const page = Number(query.page);
+  const limit = Number(query.limit);
+
+  if (query.page && isNaN(page)) {
+    res.status(400).json({
+      error: {
+        message: "`page` must be a number",
+      },
+    });
+    return;
+  }
+  if (query.limit && isNaN(limit)) {
+    res.status(400).json({
+      error: {
+        message: "`limit` must be a number",
+      },
+    });
+    return;
+  }
+
+  const output = todoRepository.get({
+    page,
+    limit,
+  });
+
+  res.status(200).json({
+    total: output.total,
+    pages: output.pages,
+    todos: output.todos,
+  });
+}
+
+const TodoCreateBodySchema = schema.object({
+  content: schema.string(),
+});
+
+async function create(req: NextApiRequest, res: NextApiResponse) {
+  // Fail Fast Validations
+  const body = TodoCreateBodySchema.safeParse(req.body);
+  // Type Narrowing
+  if (!body.success) {
+    res.status(400).json({
+      error: {
+        message: "You need to provide a content to create a TODO",
+        description: body.error.issues,
+      },
+    });
+    return;
+  }
+  // Here we have the data!
+  const createdTodo = await todoRepository.createByContent(body.data.content);
+
+  res.status(201).json({
+    todo: createdTodo,
+  });
+}
+
+export const todoController = {
+  get,
+  create,
+};
