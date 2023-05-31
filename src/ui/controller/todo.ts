f8f5f8b9a244834a5971b8f4ca93c51a39083945
victoresg <victoresg9@gmail.com>
2023-05-31 18:26:55 -0300

    UI controller and repository.

---
+++ src/ui/controller/todo.ts
@@ -0,0 +1,58 @@
+import { todoRepository } from "@ui/repository/todo";
+import { Todo } from "@ui/schema/todo";
+import { z as schema } from "zod";
+
+interface TodoControllerGetParams {
+  page: number;
+}
+
+async function get(params: TodoControllerGetParams) {
+  // Fazer a lógica de pegar os dados
+  return todoRepository.get({
+    page: params.page,
+    limit: 1,
+  });
+}
+
+function filterTodosByContent<Todo>(
+  search: string,
+  todos: Array<Todo & { content: string }>
+): Array<Todo> {
+  const homeTodos = todos.filter((todo) => {
+    const searchNormalized = search.toLowerCase();
+    const contentNormalized = todo.content.toLowerCase();
+    return contentNormalized.includes(searchNormalized);
+  });
+
+  return homeTodos;
+}
+
+interface TodoControllerCreateParams {
+  content?: string;
+  onError: (customMessage?: string) => void;
+  onSuccess: (todo: Todo) => void;
+}
+
+function create({ content, onSuccess, onError }: TodoControllerCreateParams) {
+  // Fail Fast
+  const parsedParams = schema.string().nonempty().safeParse(content);
+  if (!parsedParams.success) {
+    onError();
+    return;
+  }
+
+  todoRepository
+    .createByContent(parsedParams.data)
+    .then((newTodo) => {
+      onSuccess(newTodo);
+    })
+    .catch(() => {
+      onError();
+    });
+}
+
+export const todoController = {
+  get,
+  filterTodosByContent,
+  create,
+};
---
+++ src/ui/repository/todo.ts
@@ -0,0 +1,113 @@
+import { z as schema } from "zod";
+import { Todo, TodoSchema } from "@ui/schema/todo";
+
+interface TodoRepositoryGetParams {
+  page: number;
+  limit: number;
+}
+interface TodoRepositoryGetOutput {
+  todos: Todo[];
+  total: number;
+  pages: number;
+}
+function get({
+  page,
+  limit,
+}: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
+  return fetch(`/api/todos?page=${page}&limit=${limit}`).then(
+    async (respostaDoServidor) => {
+      const todosString = await respostaDoServidor.text();
+      // Como garantir a tipagem de tipos desconhecidos?
+      const responseParsed = parseTodosFromServer(JSON.parse(todosString));
+
+      return {
+        total: responseParsed.total,
+        todos: responseParsed.todos,
+        pages: responseParsed.pages,
+      };
+    }
+  );
+}
+
+export async function createByContent(content: string): Promise<Todo> {
+  const response = await fetch("/api/todos", {
+    method: "POST",
+    headers: {
+      // MIME Type
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content,
+    }),
+  });
+
+  if (response.ok) {
+    const serverResponse = await response.json();
+
+    const ServerResponseSchema = schema.object({
+      todo: TodoSchema,
+    });
+    const serverResponseParsed = ServerResponseSchema.safeParse(serverResponse);
+
+    if (!serverResponseParsed.success) {
+      throw new Error("Failed to create TODO :(");
+    }
+
+    const todo = serverResponseParsed.data.todo;
+    return todo;
+  }
+
+  throw new Error("Failed to create TODO :(");
+}
+
+export const todoRepository = {
+  get,
+  createByContent,
+};
+
+// Model/Schema
+
+function parseTodosFromServer(responseBody: unknown): {
+  total: number;
+  pages: number;
+  todos: Array<Todo>;
+} {
+  if (
+    responseBody !== null &&
+    typeof responseBody === "object" &&
+    "todos" in responseBody &&
+    "total" in responseBody &&
+    "pages" in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    return {
+      total: Number(responseBody.total),
+      pages: Number(responseBody.pages),
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (todo === null && typeof todo !== "object") {
+          throw new Error("Invalid todo from API");
+        }
+
+        const { id, content, done, date } = todo as {
+          id: string;
+          content: string;
+          date: string;
+          done: string;
+        };
+
+        return {
+          id,
+          content,
+          done: String(done).toLowerCase() === "true",
+          date: date,
+        };
+      }),
+    };
+  }
+
+  return {
+    pages: 1,
+    total: 0,
+    todos: [],
+  };
+}
